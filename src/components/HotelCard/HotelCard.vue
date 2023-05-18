956912e188732e97951ca0b9ead6216f08efd2bc
Leonardo Waichert <leonardowaichert@gmail.com>
2023-05-18 22:19:06 +1000

    - Adds responsive behavior on Food card

--- src/components/HotelCard/HotelCard.vue
+++ src/components/HotelCard/HotelCard.vue
@@ -130,6 +130,7 @@ const setAsFavorite = () => {
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   display: flex;
   flex-direction: column;
+  min-width: 320px;
 
   &--label {
     display: flex;
